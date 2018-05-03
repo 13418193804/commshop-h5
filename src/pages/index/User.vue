60d9ef15a37085b664125c632bc7d989c11f1e7e
472143915@qq.com <472143915@qq.com>
2018-05-03 19:00:01 +0800

    头像

--- src/pages/index/User.vue
+++ src/pages/index/User.vue
@@ -18,12 +18,8 @@
             
 
 <div style="font-size:16px;" @click="go_essential()">
-<<<<<<< HEAD
-    <img  src="../../assets/image/头像.png" style="width:100px;height:100px;border-radius: 100px;"/>
-=======
     <img v-if="userIcon" v-lazy="userIcon" style="width:100px;height:100px;border-radius: 100px;"/>
     <img v-else src="../../assets/image/头像.png" style="width:100px;height:100px;border-radius: 100px;"/>
->>>>>>> 5cfa51f004e66258250c64a30a75bb5c4d883eb7
     <div>普通用户名称</div>
 </div>
       </div>
