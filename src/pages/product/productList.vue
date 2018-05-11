fbf6b5d99df003bf80181e3642b24cc19b6f0c56
472143915@qq.com <472143915@qq.com>
2018-05-11 11:00:56 +0800

    商品列表页bug修改

--- src/pages/product/productList.vue
+++ src/pages/product/productList.vue
@@ -6,56 +6,39 @@
 <van-tabs :active="selectIndex" @click="selectCatItem">
 
   <van-tab v-for="(item,index) in catlist" :title="item.catName" :key="index">
-
+    <div v-if="index==selectIndex">
 
       <div style="background-color:#f7f7f7;height:10px;"></div>
       <div :style="handlePX('line-height',100)+handlePX('font-size',32)" style="color:#000000;text-align:center;">{{catlist[selectIndex].catName}}</div>
 
-<ul
-  v-infinite-scroll="loadMore"
-  infinite-scroll-disabled="loading"
-  infinite-scroll-distance="10">
-  <li  v-for="(item,goodsListindex) in goodsList">
+        <ul v-infinite-scroll="loadMore" :infinite-scroll-disabled="loading" infinite-scroll-distance="20" style="display:flex;justify-content:space-between;flex-wrap:wrap;margin:0 10px;">
+          <li  v-for="(item,goodsListindex) in goodsList" :key="goodsListindex">
+                  <div class="bodyItem" :style="handlePX('margin-bottom',50)" style="display:flex;justify-content:center;" @click="goProductDetail(item.goodsId)">
+                  <div style="overflow: hidden;">
+                    <div style="border: 1px #e5e5e5 solid;box-sizing: border-box;display:flex;align-items: center;justify-content:center;overflow:hidden;position:relative;margin:5px auto;" :style="handlePX('height', 410)+handlePX('width', 345)">
+                      <img src="../../assets/image/热.png" style="width:-webkit-fill-available;position: absolute;top: 0;left:0;" :style="handlePX('width', 43)+handlePX('height', 49)"/>
+                      <img v-lazy="item.goodsImg.split(',')[0]" style="width:-webkit-fill-available;position: absolute;top: 0;z-index:-1;"/>
+                      <div class="textLabel" style="position: absolute;bottom: 0;width: 100%;background-color:rgba(207,207,207,0.3);text-align:center;color:#A3A3A3" :style="handlePX('height', 70)+handlePX('line-height', 70)+handlePX('font-size', 28)">{{item.jingle}}</div>
+                    </div>
+                    <div style="margin:0 5px;">                      
+                    <div>
+                      <img src="../../assets/image/满减.png" :style="handlePX('width',52)+handlePX('height',25)"/>
+                      <img src="../../assets/image/特价.png" :style="handlePX('width',52)+handlePX('height',25)"/>
+                    </div>                  
+                    <div class="textLabel" :style="handlePX('font-size',28)" style="color:#000000;">{{item.goodsName}}</div>
+                    <div :style="handlePX('font-size',30)" style="color:#E05459;">￥{{item.marketPrice}}</div>
+                    </div>
+                  </div>
+                </div>
+          </li>
+        </ul>
 
-
-      <van-row  >
-        <van-col span="12">
-          <div class="bodyItem" :style="handlePX('margin-bottom',50)" style="display:flex;justify-content:center;" @click="goProductDetail(item.goodsId)">
-          <div style="overflow: hidden;">
-            <div style="border: 1px #e5e5e5 solid;box-sizing: border-box;display:flex;align-items: center;justify-content:center;overflow:hidden;position:relative;margin:5px auto;" :style="handlePX('height', 410)+handlePX('width', 345)">
-              <img src="../../assets/image/热.png" style="width:-webkit-fill-available;position: absolute;top: 0;left:0;" :style="handlePX('width', 43)+handlePX('height', 49)"/>
-              <img v-lazy="item.goodsImg.split(',')[0]" style="width:-webkit-fill-available;position: absolute;top: 0;z-index:-1;"/>
-              <div class="textLabel" style="position: absolute;bottom: 0;width: 100%;background-color:rgba(207,207,207,0.3);text-align:center;color:#A3A3A3" :style="handlePX('height', 70)+handlePX('line-height', 70)+handlePX('font-size', 28)">{{item.jingle}}</div>
-            </div>
-            <div style="margin:0 5px;">
-              
-            <div>
-              <img src="../../assets/image/满减.png" :style="handlePX('width',52)+handlePX('height',25)"/>
-              <img src="../../assets/image/特价.png" :style="handlePX('width',52)+handlePX('height',25)"/>
-            </div>
-           
-            <div class="textLabel" :style="handlePX('font-size',28)" style="color:#000000;">{{item.goodsName}}</div>
-            <div :style="handlePX('font-size',30)" style="color:#E05459;">￥{{item.marketPrice}}</div>
-            </div>
-          </div>
+        <div style="display: flex;align-items: center;justify-content: center;font-size:14px;padding:15px;">
+            <div v-if="loading">加载中...</div>
+            <div v-else>没有更多</div>
         </div>
-        </van-col>
-        </van-row>
-        </van-col>
-   </li>
-</ul>
 
-
-<div style="    display: flex;
-    align-items: center;
-    justify-content: center;font-size:14px;padding:15px;">
-
-    <div >加载中...</div>
-    <div>-</div>
-  
-</div>
-
-
+    </div>
 
 
   </van-tab>
@@ -93,7 +76,7 @@ export default class ProductList extends Vue {
   goodsList = [];
   catId='';
   parentCatId="";
-  pageIndex=20;
+  pageIndex=0;
   title="";
   goProductDetail(goodsId) {
     this.$router.push({
@@ -107,7 +90,7 @@ export default class ProductList extends Vue {
     let item = this.catlist[index];
     if(item['catId']!=this.catId){
       this.goodsList = []
-      this.pageIndex = 20;
+      this.pageIndex = 0;
     }
     this.catId = item['catId'];
     this.selectIndex = index;
@@ -123,15 +106,22 @@ export default class ProductList extends Vue {
     });
   }
   loadMore(){
-
+    console.log("刷新");
+    this.loading = true;
+    let self = this;
+    setTimeout(() => {
+      self.pageIndex = self.pageIndex + 1;
+      self.getProductList();
+      self.loading = false;
+    }, 1000);
   }
   getProductList() {
     Vue.prototype.$reqFormPost(
       "/user/goods/list",
       {
         catId: this.catId,
-        page: 0,
-        pageSize:this.pageIndex,
+        page: this.pageIndex,
+        pageSize:20,
       },
       res => {
         if (res == null) {
@@ -144,31 +134,20 @@ export default class ProductList extends Vue {
           );
           return;
         }        
-
-        if(this.pageIndex==0){
-          this.goodsList = res.data.data.goodsList;
-        }else{
-          for (let i = 0; i < res.data.data.goodsList.length; i++) {
-            this.goodsList.push(res.data.data.goodsList[i])
-          }
+        let goodsList = this.goodsList ? this.goodsList : [];
+        for (let i = 0; i < res.data.data.goodsList.length; i++) {
+          goodsList.push(res.data.data.goodsList[i]);
         }
-        
+        if (res.data.data.goodsList.length == 20) {
+          this.loading = false;
+        } else {
+          // this.finished = true;
+        }
+        this.goodsList = goodsList;
+        console.log(this.finished);        
       }
     );
   }
-
-  onLoad() {
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1);
-        }
-        this.loading = false;
-
-        if (this.list.length >= 40) {
-          this.finished = true;
-        }
-      }, 500);
-    }
   getCategoryList(){
      Vue.prototype.$reqFormPost("/user/cat/list", {
       parentId:  this.parentCatId
