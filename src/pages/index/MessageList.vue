9a72d5d05ff48276e2d5822585d7468e833a0df0
472143915@qq.com <472143915@qq.com>
2018-05-28 16:34:10 +0800

    消息中心

--- src/pages/index/MessageList.vue
+++ src/pages/index/MessageList.vue
@@ -1,8 +1,18 @@
 <template>
-  <div class="messagePage">
+  <div class="messagePage" style="background-color: #f3f3f3;">
 
     <comhead ref="comhead" isLeftIcon="icon-zuo" leftIconName="angle-left" @leftClick="false"  title="消息中心" isRightIcon="true"  ></comhead>
-
+     <ul
+  v-infinite-scroll="loadMore"
+  :infinite-scroll-disabled="loading"
+  infinite-scroll-distance="20" >
+        <li v-for="(item,index) in messagelist" :key="index">
+          <div style="display:flex;justify-content: space-between;border-bottom:1px solid #f4f4f4;background-color: #fff;" :style="handlePX('padding',20)">
+            <div style="font-size:12px;color:#a9a9a9;">{{item.content}}</div>
+            <div style="font-size:12px;color:#a9a9a9;text-align: right;vertical-align: middle;" :style="handlePX('width',300)">{{item.updateTime}}</div> 
+          </div>
+        </li>
+ </ul>
   </div>
 </template>
 
@@ -29,9 +39,63 @@ import comhead from "../../components/Comhead.vue";
   mixins: [mixin]
 })
 export default class shopIndex extends Vue {
-  
+  loading=false
+  pageSize = 20;
+  messagelist=[];
+  page=0;
+  loadMore(){
+    let self = this;
+    self.loading=true;    
+    setTimeout(() => {
+      if(!self.loading){
+        self.page+=1;
+        self.getList();
+        self.loading = false;
+      }
+    }, 1000);
+  }
+  getList() {
+ Vue.prototype.$reqFormPost("/message/list", {
+   
+      userId: this.$store.getters[Vue.prototype.MutationTreeType.TOKEN_INFO]
+        .userId,
+      token: this.$store.getters[Vue.prototype.MutationTreeType.TOKEN_INFO]
+        .token,
+      page: this.page,
+      pageSize: 20
+ }, res => {
+      if (res == null) {
+        console.log("网络请求错误！");
+        return;
+      }
+      if (res.data.status != 200) {
+        console.log(
+          "需控制错误码" + res.data.status + ",错误信息：" + res.data.message
+        );
+        Toast(res.data.message);
+        return;
+      }
+    console.log(res)
+    this.messagelist = res.data.data.messageList;
+       if (res.data.data.messageList.length == 20) {
+          this.loading = false;
+        }
+    });
+
+
+  }
+  handlePX(CssName, PxNumber) {
+    return (
+      CssName +
+      ":" +
+      this.$store.getters[Vue.prototype.MutationTreeType.SYSTEM].availWidth /
+        750 *
+        PxNumber +
+      "px;"
+    );
+  }
   mounted() {
-    
+    this.getList();
   }
 }
 </script>
